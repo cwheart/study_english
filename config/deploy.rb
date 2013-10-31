264c22b5e9722c5523f856ab9a719de0d05f7550
xin <xinshuaifeng@126.com>
2013-10-31 18:18:32 +0800

    study init

---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+set :application, 'blog'
+set :repo_url, 'git@github.com:cwheart/study_english.git'
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# set :deploy_to, '/var/www/my_app'
+set :scm, :git
+
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+set :linked_files, %w{config/mongoid.yml config/unicorn.rb config/puma.rb}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets tmp/states vendor/bundle public/system}
+
+set :keep_releases, 5
+
+namespace :deploy do
+
+  desc "Start application"
+  task :start do
+    on roles(:app) do
+      execute "cd #{deploy_to}/current/ && bundle exec puma -C config/puma.rb"
+    end
+  end
+ 
+  desc'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+       execute "cd #{deploy_to}/current/ && bundle exec pumactl -S tmp/states/puma.state restart"
+    end
+  end
+
+  desc "Stop application"
+  task :stop do
+    on roles(:app) do
+      execute "cd #{deploy_to}/current/ && bundle exec pumactl -S tmp/states/puma.state stop"
+    end
+  end
+
+  desc "Stop application force"
+  task :force_stop do
+    on roles(:app) do
+      execute "cd #{deploy_to}/current/ && bundle exec pumactl -S tmp/states/puma.state halt"
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular study_englishs or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudyEnglish::Application.config.secret_key_base = 'c265742d17c8f9c79c61730914f82f2cc0d325fbd5716b7960d8f22c72046358d36237635767fc0445046a8681b64621751b247c5a5f1407c78cf8a38fcf63fa'
