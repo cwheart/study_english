4d7fffc93a81cecb219bab8e94b471ae78a17e35
xin <xinshuaifeng@126.com>
2013-11-01 10:55:43 +0800

    res words

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+File.open("res/word.txt", 'r') do |f|
+  words = []
+  while line=f.gets
+    arr = line.strip.split(/[\[\]\/]/)
+    next if arr.empty?
+    words.push({name: arr[0].strip, pronounce: arr[1].strip, content: arr[2].strip})
+    if words.count >= 200
+      Word.create(words)
+      words = []
+    end
+  end
+  Word.create(words)
+end
